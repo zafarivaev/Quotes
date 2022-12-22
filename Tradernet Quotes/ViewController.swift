3522fe8caa22e1a2967be85c6280361939490dba
zafarivaev <z.ivaev@mail.ru>
2022-12-22 19:36:24 +0500

    Initial Commit

---
+++ Tradernet Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tradernet Quotes
+//
+//  Created by Zafar Ivaev on 22/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
